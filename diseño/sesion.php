0bd51917c3d81be43d069c178f852c9d36d2ca27
XoelGarcia <xgarciat@fpcoruna.afundacion.org>
2022-06-04 11:06:31 +0200

    Update

--- diseño/sesion.php
+++ diseño/sesion.php
@@ -21,17 +21,38 @@
     <title>BitShop</title>
 </head>
 <body>
-    <header>
-        <div class="logo"><img src="/imagenes/logo_bitwear.png" alt=""></div>
-        <div class="search">
-            <input type="text" id="idbusqueda" placeholder="Buscador">
-            <button class="btn-main btn-search"><i class="fa fa-search" aria-hidden="true"></i></button>
-        </div>
-        <div class="options-place">
-            <div class="item-option"><a href="logout.php"><i class="fa fa-sign-in" aria-hidden="true"></i></a></div>
-            <div class="item-option"><a href="vista.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a></div>
+<header>
+        <div class="row fixed-top">
+            <div class="col">
+            <img src="imagenes/logo_bitwear.png" alt="" width="300px" id="logo">
+            </div>
+            <div class="col text-end">
+            <a href="logout.php"><img src="imagenes/logout.png" alt="LOGOUT" width="50px" id="iconos"></a>
+            <a href="carrito.php"><img src="imagenes/carrito.png" alt="CARRITO" width="50px" id="iconos"></a>
+            </div>
         </div>
     </header>
+    <!-- corousel -->
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="imagenes/bitcoin_carousel.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="imagenes/bitcoin_carousel.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <!-- Productos -->
+    <div class="container">
     <?php
             echo '<br>';
             echo '<br>';
@@ -53,6 +74,83 @@
             #Cerramos la conexión
             mysqli_close($mysqli);
         ?>
+
+    </div>
+
+    <footer>
+       
+       <div class="container-footer-all">
+        
+            <div class="container-body-footer">
+
+                <div class="colum1-footer">
+                    <h1>Mas informacion de la compañia</h1>
+
+                    <p>Web creada por Xoel García Tarrío, esta web consiste en venta de productos y pagos online con criptomonedas.</p>
+
+                </div>
+
+                <div class="colum2-footer">
+
+                    <h1>Redes Sociales</h1>
+
+                    <div class="row-footer">
+                        <img src="icon/facebook.png">
+                        <label>Siguenos en Facebook</label>
+                    </div>
+                    <div class="row-footer">
+                        <img src="icon/twitter.png">
+                        <label>Siguenos en Twitter</label>
+                    </div>
+                    <div class="row-footer">
+                        <img src="icon/instagram.png">
+                        <label>Siguenos en Instagram</label>
+                    </div>
+
+                </div>
+
+                <div class="colum3-footer">
+
+                    <h1>Informacion Contactos</h1>
+
+                    <div class="row2-footer">
+                        <img src="icon/house.png">
+                        <label>Laracha</label>
+                    </div>
+
+                    <div class="row2-footer">
+                        <img src="icon/smartphone.png">
+                        <label>+34-698110644</label>
+                    </div>
+
+                    <div class="row2-footer">
+                        <img src="icon/contact.png">
+                         <label>xoelgarcia27@gmail.com</label>
+                    </div>
+
+                </div>
+
+            </div>
+        
+        </div>
+        
+        <div class="container-footer">
+               <div class="footer">
+                    <div class="copyright">
+                        © 2022 Todos los Derechos Reservados | <a href="">Xoel</a>
+                    </div>
+
+                    <div class="information">
+                        <a href="">Informacion Compañia</a> | <a href="">Privacion y Politica</a> | <a href="">Terminos y Condiciones</a>
+                    </div>
+                </div>
+
+            </div>
+        
+    </footer>
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
 </body>
